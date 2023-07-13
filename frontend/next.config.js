63b6bee8f83b3226bbffe15797a88275e1a96de3
kareldecoene <info.presentonline@gmail.com>
2023-07-13 16:24:44 +0200

    another test for the images adding https

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["localhost", "127.0.0.1", "fortunate-victory-22b45df4b4.strapiapp.com"],
+		domains: ["localhost", "127.0.0.1", "https://fortunate-victory-22b45df4b4.strapiapp.com"],
 	},
 };
 
